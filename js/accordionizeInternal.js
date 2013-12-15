1f96e6dff10fe4385424154c4aeb7595b6acec4a
alekseyleshko <alekseyleshko@gmail.com>
2013-12-15 04:19:51 +0400

    Implemented events

--- js/accordionizeInternal.js
+++ js/accordionizeInternal.js
@@ -40,6 +40,7 @@ Accordionize.prototype.createPlaginDomTree = function() {
   }
 
   this.paintOnCanvas();
+  this.setEvents();
 }
 
 Accordionize.prototype.convertImgToPluginSlide = function(imgArray) {
@@ -99,4 +100,108 @@ Accordionize.prototype.grayscale = function(imageData) {
     var grayscale = pix[i] * .3 + pix[i + 1] * .59 + pix[i + 2] * .11;
     pix[i] = pix[i + 1] = pix[i + 2] = grayscale;
   }
+}
+
+Accordionize.prototype.getSlideWidth = function() {
+  var slideWidth = this.$plaginContainer.width() -
+    (this.plaginOptions.tabWidth + 1) * (this.slides.length - 1) - 1;
+
+  return slideWidth;
+}
+
+Accordionize.prototype.setEvents = function() {
+  var $elements = this.$plaginContainer.find('.banner-item'),
+      slideWidth = this.getSlideWidth();
+
+  var _this = this;
+  for (var i = 0; i < $elements.length; i++) {
+    var $element = $($elements[i]);
+
+    $element.on('mousedown', function() {
+      var $this = $(this);
+      if (!$this.is('.active')) {
+        if (_this.$plaginContainer.attr('data-animated') != 'true') {
+          _this.$plaginContainer.attr('data-animated', 'true');
+          _this.wrap(_this, $this.siblings('.active'));
+          _this.unwrap(_this, $this, slideWidth);
+          _this.setLoop(_this);
+        } else {
+          return false;
+        }
+      }
+    });
+  }
+}
+
+Accordionize.prototype.setLoop = function(_this) {
+  if(_this.plaginOptions.scroll.auto) {
+    clearInterval(window.interval);
+
+    window.interval = setInterval(function() {
+      var $activeElement = _this.$plaginContainer.children('.active');
+      if($activeElement.is(':last-child')) {
+        _this.$plaginContainer
+          .children('.banner-item:first-child')
+          .trigger('mousedown');
+      } else {
+        $activeElement
+          .next()
+          .trigger('mousedown');
+      }
+    }, _this.plaginOptions.scroll.timeout);
+  }
+}
+
+Accordionize.prototype.unwrap = function(_this, $slide, slideWidth) {
+  setTimeout(function() {
+    _this.setDataForAnimate(_this, $slide, slideWidth);
+    $slide.addClass('active');
+
+    $slide
+      .find('.banner-item-overlay')
+      .fadeOut(400);
+
+    setTimeout(function() {
+      _this.fadeInElementsForUnwrap($slide);
+    }, 400)
+  }, 300);
+}
+
+Accordionize.prototype.wrap = function(_this, $slide) {
+  setTimeout(function() {
+    _this.setDataForAnimate(_this, $slide, _this.plaginOptions.tabWidth);
+    $slide.removeClass('active');
+
+    setTimeout(function() {
+      _this.fadeInElementsForWrap($slide);
+    }, 400);
+  }, 300);
+
+  $slide
+    .find('.banner-item-label')
+    .fadeOut(300);
+}
+
+Accordionize.prototype.setDataForAnimate = function(_this, $slide, width) {
+  $slide.animate({ 'width': width + 'px' }, 400, function() {
+    _this.$plaginContainer.attr('data-animated', 'false');
+  });
+}
+
+Accordionize.prototype.fadeInElementsForUnwrap = function($slide) {
+  $slide
+    .find('.banner-item-label')
+    .fadeIn(300);
+  $slide
+    .find('img')
+    .fadeIn(300);
+}
+
+Accordionize.prototype.fadeInElementsForWrap = function($slide) {
+  $slide
+    .find('img')
+    .fadeOut(300);
+  $slide
+    .find('.banner-item-overlay')
+    .fadeIn(400);
 }
